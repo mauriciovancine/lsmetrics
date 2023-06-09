2925959b74d69003d043f337c5603387ee0db5c5
mauriciovancine <mauricio.vancine@gmail.com>
2023-06-09 13:06:45 -0300

    add lsm_perimeter function

---
+++ R/lsm_perimeter.R
@@ -0,0 +1,82 @@
+#' Calculate perimeter
+#'
+#' Calculate perimeter in meter.
+#'
+#' @param input `[character=""]` \cr Habitat map, following a binary classification
+#' (e.g. values 1,0 or 1,NA for habitat,non-habitat) inside GRASS Data Base.
+#' @param output `[character=""]` \cr Map name output inside GRASS Data Base.
+#' @param zero_as_na `[logical(1)=FALSE]` \cr If `TRUE`, the function treats
+#' non-habitat cells as null; if `FALSE`, the function converts non-habitat zero
+#' cells to null cells.
+#'
+#' @example examples/lsm_perimeter_example.R
+#'
+#' @name lsm_perimeter
+#' @export
+lsm_perimeter <- function(input,
+                          output = NULL,
+                          zero_as_na = FALSE){
+
+    # binary
+    if(zero_as_na == TRUE){
+
+        # patch id
+        rgrass::execGRASS(cmd = "g.message", message = "Identifying the patches")
+        rgrass::execGRASS(cmd = "r.mapcalc", flags = "overwrite",
+                          expression = paste0(input, output, "_perimeter_bin = if(", input, " == 1, 1, 0)"))
+        rgrass::execGRASS(cmd = "r.mapcalc", flags = "overwrite",
+                          expression = paste0(input, output, "_perimeter_null =", input, output))
+    } else{
+
+        # null
+        rgrass::execGRASS(cmd = "g.message", message = "Converting zero as null")
+        rgrass::execGRASS(cmd = "r.mapcalc", flags = "overwrite",
+                          expression = paste0(input, output, "_perimeter_bin =", input, output))
+        rgrass::execGRASS(cmd = "r.mapcalc", flags = "overwrite",
+                          expression = paste0(input, output, "_perimeter_null = if(", input, " == 1, 1, null())"))
+    }
+
+    # matrix ----
+    rgrass::execGRASS(cmd = "r.mapcalc", flags = "overwrite",
+                      expression = paste0(input, output, "_perimeter_matrix = if(", input, output, "_perimeter_bin == 1, 0, 1)"))
+
+    # count edge to matrix
+    rgrass::execGRASS(cmd = "r.neighbors",
+                      flags = c("c", "overwrite"),
+                      input = paste0(input, output, "_perimeter_matrix"),
+                      selection = input,
+                      output = paste0(input, output, "_perimeter_count_edges"),
+                      size = 3,
+                      method = "sum")
+
+    res_pixel <- as.numeric(gsub(".*?([0-9]+).*", "\\1", grep("nsres", rgrass::stringexecGRASS("g.region -p", intern=TRUE), value = TRUE)))
+    rgrass::execGRASS(cmd = "r.mapcalc",
+                      flags = "overwrite",
+                      expression = paste0(input, output, "_perimeter_count_edges = ", input, output, "_perimeter_count_edges * ", res_pixel, " * ", input, output, "_perimeter_null"))
+
+    # id
+    rgrass::execGRASS(cmd = "g.message", message = "Identifying the patches")
+    rgrass::execGRASS(cmd = "r.clump",
+                      flags = c("d", "quiet", "overwrite"),
+                      input = paste0(input, output, "_perimeter_null"),
+                      output = paste0(input, output, "_perimeter_id"))
+
+   # perimeter
+    rgrass::execGRASS(cmd = "r.stats.zonal",
+                      flags = c("overwrite"),
+                      base = paste0(input, output, "_perimeter_id"),
+                      cover = paste0(input, output, "_perimeter_count_edges"),
+                      method = "sum",
+                      output = paste0(input, output, "_perimeter"))
+
+    # color
+    rgrass::execGRASS(cmd = "r.colors", flags = c("quiet"), map = paste0(input, output, "_perimeter"), color = "bgyr")
+
+    # clean
+    rgrass::execGRASS(cmd = "g.message", message = "Cleaning rasters")
+    rgrass::execGRASS(cmd = "g.remove", flags = c("b", "f", "quiet"), type = "raster", name = paste0(input, output, "_perimeter_id"))
+    rgrass::execGRASS(cmd = "g.remove", flags = c("b", "f", "quiet"), type = "raster", name = paste0(input, output, "_perimeter_null"))
+    rgrass::execGRASS(cmd = "g.remove", flags = c("b", "f", "quiet"), type = "raster", name = paste0(input, output, "_perimeter_count_edges"))
+    rgrass::execGRASS(cmd = "g.remove", flags = c("b", "f", "quiet"), type = "raster", name = paste0(input, output, "_perimeter_matrix"))
+
+}
---
+++ examples/lsm_perimeter_example.R
@@ -0,0 +1,44 @@
+library(lsmetrics)
+library(terra)
+
+# read habitat data
+f <- system.file("raster/toy_landscape_habitat.tif", package = "lsmetrics")
+r <- terra::rast(f)
+
+# plot
+plot(r, legend = FALSE, axes = FALSE, main = "Binary habitat")
+plot(as.polygons(r, dissolve = FALSE), lwd = .1, add = TRUE)
+plot(as.polygons(r), add = TRUE)
+text(r)
+
+# find grass
+path_grass <- as.character(link2GI::findGRASS()[1]) # windows users need to find, e.g. "C:/Program Files/GRASS GIS 8.2"
+
+# create grassdb
+rgrass::initGRASS(gisBase = path_grass,
+                  SG = r,
+                  gisDbase = "grassdb",
+                  location = "newLocation",
+                  mapset = "PERMANENT",
+                  override = TRUE)
+
+# import raster from r to grass
+rgrass::write_RAST(x = r, flags = c("o", "overwrite", "quiet"), vname = "r", verbose = FALSE)
+
+# perimeter
+lsmetrics::lsm_perimeter(input = "r", zero_as_na = FALSE)
+
+# files
+# rgrass::execGRASS(cmd = "g.list", type = "raster")
+
+# import from grass to r
+r_perimeter <- terra::rast(rgrass::read_RAST("r_perimeter", flags = "quiet", return_format = "SGDF"))
+
+# plot
+plot(r_perimeter, legend = FALSE, axes = FALSE, main = "Perimeter (m)")
+plot(as.polygons(r, dissolve = FALSE), lwd = .1, add = TRUE)
+plot(as.polygons(r), add = TRUE)
+text(r_perimeter, cex = .5)
+
+# delete grassdb
+unlink("grassdb", recursive = TRUE)

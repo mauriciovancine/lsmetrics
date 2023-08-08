0bf0ad76e8d4cc6200e5d266b4576e0cd646e438
mauriciovancine <mauricio.vancine@gmail.com>
2023-08-08 19:38:16 -0300

    update lsm_diversity

--- R/lsm_diversity.R
+++ R/lsm_diversity.R
@@ -31,7 +31,7 @@ lsm_diversity <- function(input,
     ## set the home path ----
     if(Sys.info()["sysname"] == "windows"){
         grass_config_dirname <- "GRASS8"
-        grass_config_dir <- file.path(Sys.getenv("APPDATA"), grass_config_dirname)
+        grass_config_dir <- file.path(Sys.getenv("APPDATA"), grass_config_dirname, fsep = "\\")
     } else{
         grass_config_dirname <- ".grass8"
         grass_config_dir <- file.path(Sys.getenv("HOME"), grass_config_dirname)
